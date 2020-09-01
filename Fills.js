02e203bb3d296d97b7f9794ab6cef70d72d3b48a
unknown <fico.zd@hotmail.com>
2020-09-01 22:05:10 +0200

    dlete

--- Fills.js
+++
@@ -1,8 +0,0 @@
-import Fill from "./Fill.js";
-import React, { Component } from "react";
-
-export default class Fills extends Component {
-  render() {
-    return this.props.fills.map((fill) => <Fill fillObj={fill} />);
-  }
-}
